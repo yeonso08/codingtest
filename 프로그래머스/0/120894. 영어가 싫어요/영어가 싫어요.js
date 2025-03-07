d7a6fc638531f45e4a42b6dff8a064aee0a3f9d5
yeonso08 <hjng0825@gmail.com>
2025-03-07 10:27:05 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,20 @@
+function solution(numbers) {
+    const wordNumbers = numbers.match(/zero|one|two|three|four|five|six|seven|eight|nine/g);
+    
+    const numMap = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    };
+    
+    let numStr = wordNumbers.map(word => numMap[word]).join('');
+    
+    return +numStr;
+}
