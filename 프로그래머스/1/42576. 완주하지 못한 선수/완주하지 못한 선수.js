45d0692d9c9782c76e104f2b2b70b817229cc596
yeonso08 <hjng0825@gmail.com>
2026-02-20 07:29:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 117.16 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,15 @@
 function solution(participant, completion) {
-    const participantSort = participant.sort();
-    const completionSort = completion.sort();
+    var answer = '';
     
-    for(let i = 0; i < participantSort.length; i++) {
-        if(participantSort[i] !== completionSort[i]) {
-            return participantSort[i];
+    let name = participant.sort();
+    let complet_name = completion.sort();
+    
+    for(let i = 0; i < name.length; ++i) {
+        if(name[i] !== complet_name[i]) {
+            answer = name[i];
+            return answer;
         }
     }
+    
+    return answer;
 }
